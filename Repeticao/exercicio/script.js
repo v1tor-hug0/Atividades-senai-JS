912b6517d83e90870f2e2e8b753889ca393deadc
vitor <ivitoror@gmail.com>
2025-09-10 10:45:03 -0300

    Terceiro e quarto exercicio

--- Repeticao/exercicio/script.js
+++ Repeticao/exercicio/script.js
@@ -44,7 +44,7 @@
 // chute = parseInt(prompt("Adivinhe o número sorteado entre 1 e 100:"));
 
 // while (chute != numero) {
-    
+
 //      if (chute > numero) {
 //     chute = parseInt(prompt("Numero Menor tente novamente"));
 // } 
@@ -62,14 +62,14 @@
 
 // while( n >= 0)
 // {
-//     n--
+//     --n
 //     console.log(n)
 // }
 
 
 //!Primeiro avancado
 
-// let numero = parseInt(prompt("Digite um número inteiro positivo:"));
+// let numero = parseInt(prompt("Digite um número "));
 // let soma = 0;
 
 // while (numero > 0) {
@@ -86,13 +86,60 @@
 // let resultado = 1;
 // let fatorial = 1;
 
+// for(let i = 1; i < n; i++){
+//     fatorial *= i;
+// } 
+// alert(`O fatorial eh ${fatorial}`)
+
 // while (n >= fatorial) {
 //     resultado = resultado * fatorial;
 //     fatorial++;
-    
 // }
 // alert(resultado);
 
 
 //!Terceiro avancado
 
+// let n = parseInt(prompt("Digite um numero"));
+// let invertido = 0;
+// let flag_negativo = false
+
+// if( n < 0) {
+//     n *= -1;
+//     flag_negativo = true;
+// }
+
+
+// while (n > 0)
+// {
+//     let unidade = n % 10;
+//     invertido = unidade + invertido * 10
+//     n = (n - unidade) / 10
+
+// }
+
+// if(flag_negativo) {
+//     invertido *= -1
+// }
+
+// alert ("A inversao eh" +invertido)
+
+
+//! Quarto avancado
+
+// let numero = parseInt(prompt("Digite um numero"));
+
+// for (let n = 1; n <= numero; n++) {
+//     let soma = 0;
+
+//    for (let i = 1; i < n; i++) {
+//         if (n % i === 0) {
+//             soma += i;
+//         }
+//     }
+
+//     if (soma === n) {
+//         lista = [n]
+//         console.log(lista + " é um número perfeito");
+//     }
+// }
